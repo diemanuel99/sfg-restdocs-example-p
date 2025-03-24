bf8186da17d2cd2e828313a0c193e7d6279758f2
Diego Emanuel Duarte Cardozo <dduarte@roshka.com>
2025-03-24 15:45:00 -0300

    add jsonformat beerdto

--- src/main/java/roshka/diegoduarte/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/roshka/diegoduarte/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
@@ -16,23 +17,34 @@ import java.util.UUID;
 public class BeerDto {
     @Null
     private UUID id;
+
     @Null
     private Integer version;
+
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
+
     @Null
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
+
     @NotBlank
     @Size(min=3, max=100)
     private String beerName;
+
     @NotNull
     private BeerStyleEnum beerStyle;
+
     @Positive
     @NotNull
     private Long upc;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
+
     @Positive
     private Integer quantityOnHand;
 }
